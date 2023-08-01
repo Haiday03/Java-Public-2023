b2651ec90eaa7b1af10209d49fe622bd7f57ccf9
unknown <vanhai200345@gmail.com>
2023-08-01 21:13:18 +0700

    test_2

---
+++ On_Tap_Co_Ban/src/Demo/Test.java
@@ -0,0 +1,24 @@
+package Demo;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		//Ép kiểu có 2 loại ép kiểu là ép kiểu ngầm định và ép kiểu tường mình
+		//Ép kiểu ngầm định là
+		int a = 4;
+		double b = a;
+		//Ép kiểu tường minh là
+		double c = 3.14;
+		int d = (int)c;
+		//Các kiểu dữ liệu là byte, int, float, double, String, char, long long,Boolean, Short
+		//Kiểu dưx liệu thì lại đc chia thành 2 loại kiểu dữ liệu là kiểu dữ liệu nguyên thủy và kiểu dữ liệu đối tượng
+		
+		//Cách đặt tên thì có phân biệt chữ hoa và chữ thường: không đc đặt tên trùng vs từ khóa, không đc đặt tên có số ở đằng trc, không đc đặt tên có dấu '-' trong tên
+		//Các phép toán tử là toán tử logic, toán tử so sánh và toán tử gán
+		
+		//Rẽ nhánh thì có if else  switch case 
+		
+		//Các vòng lặp thì có vòng for do while while 
+		
+	}
+}
